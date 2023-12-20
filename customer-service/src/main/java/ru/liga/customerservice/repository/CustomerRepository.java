8ecef007ea52e4b957f2dad65e640e6889e8191f
Mukhin Sergey <segam@inbox.ru>
2023-12-20 23:21:42 +0300

    Update food-Service

--- customer-service/src/main/java/ru/liga/customerservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.liga.customerservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.liga.common.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
