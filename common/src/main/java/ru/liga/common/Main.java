426c5aba26abf112b5b52971a575a7907098e85b
Mukhin Sergey <segam@inbox.ru>
2024-01-08 13:45:48 +0300

    Update common

---
+++ common/src/main/java/ru/liga/common/Main.java
@@ -0,0 +1,10 @@
+package ru.liga.common;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+
+@EnableRabbit
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
